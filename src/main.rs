b08d92d4fb89c5be01f2fbdae44858f8803f6e36
Vaurkhorov <sssamantaray77@gmail.com>
2024-11-30 16:02:52 +0530

    added market and server tasks
    price now stored as int and converted to float when displayed
    
    server uses std::io right now for testing

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,224 @@
 mod types;
-use types::{Stock, Order, Trade};
-// use tokio::sync::mpsc;
 
-
+use std::collections::HashMap;
 use std::io;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use tokio::sync::mpsc::error::TryRecvError;
+use tokio::task;
+use tokio::sync::mpsc;
+use tokio::time;
+use types::{Order, Query, QueryResponse, Stock};
 
-fn main() {
-    println!("D1X Demo - running with one stock");
-    let mut v = Stock::new("V", "Vulyenne");
+const TICK_INTERVAL_MILLISECS: u64 = 10;
+const MARKET_OUTPUT_COLOUR: Color = Color::Yellow;
 
-    let mut input = String::new();
-    println!("{}({}): {:#?}", v.get_name(), v.get_symbol(), v.get_ohlc());
+#[tokio::main]
+async fn main() {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
 
-    loop {
-        println!("Enter a command: ");
-        input.clear();
-        io::stdin().read_line(&mut input).unwrap();
-        let input = input.trim();
+    let (server_tx, mut market_rx) = mpsc::channel::<Query>(32);
+    let (market_tx, mut server_rx) = mpsc::channel::<QueryResponse>(32);
 
-        match input {
-            "buy" => {
-                println!("Enter creator id: ");
-                let mut creator_id = String::new();
-                io::stdin().read_line(&mut creator_id).unwrap();
-                let creator_id: usize = creator_id.trim().parse().unwrap();
+    let mut stocks: HashMap<String, Stock> = HashMap::new();
+    let mut initial_stocks = vec![
+        Stock::new("V", "Vulyenne"),
+    ];
 
-                println!("Enter price: ");
-                let mut price = String::new();
-                io::stdin().read_line(&mut price).unwrap();
-                let price: f64 = price.trim().parse().unwrap();
+    while let Some(stock) = initial_stocks.pop() {
+        stocks.insert(stock.get_symbol().to_string(), stock);
+    }
 
-                println!("Enter quantity: ");
-                let mut quantity = String::new();
-                io::stdin().read_line(&mut quantity).unwrap();
-                let quantity: usize = quantity.trim().parse().unwrap();
+    let server = task::spawn(async move {
+        let mut input = String::new();
+    
+        loop {
+            println!("Enter a command: ");
+            input.clear();
+            io::stdin().read_line(&mut input).unwrap();
+            let input = input.trim();
+    
+            let status = match input {
+                "buy" => {
+                    println!("Enter stock symbol: ");
+                    let mut symbol = String::new();
+                    io::stdin().read_line(&mut symbol).unwrap();
+                    let symbol: String = symbol.trim().parse().unwrap();
+    
+                    println!("Enter creator id: ");
+                    let mut creator_id = String::new();
+                    io::stdin().read_line(&mut creator_id).unwrap();
+                    let creator_id: usize = creator_id.trim().parse().unwrap();
+    
+                    println!("Enter price: ");
+                    let mut price = String::new();
+                    io::stdin().read_line(&mut price).unwrap();
+                    let price: f64 = price.trim().parse().unwrap();
+    
+                    println!("Enter quantity: ");
+                    let mut quantity = String::new();
+                    io::stdin().read_line(&mut quantity).unwrap();
+                    let quantity: usize = quantity.trim().parse().unwrap();
+    
+                    let mpsc_status = server_tx.send(Query::Buy(symbol, Order::new(creator_id, price, quantity))).await;
+                    println!("Buy order added.");
+                    mpsc_status
+                }
+                "sell" => {
+                    println!("Enter stock symbol: ");
+                    let mut symbol = String::new();
+                    io::stdin().read_line(&mut symbol).unwrap();
+                    let symbol: String = symbol.trim().parse().unwrap();
+    
+                    println!("Enter creator id: ");
+                    let mut creator_id = String::new();
+                    io::stdin().read_line(&mut creator_id).unwrap();
+                    let creator_id: usize = creator_id.trim().parse().unwrap();
+    
+                    println!("Enter price: ");
+                    let mut price = String::new();
+                    io::stdin().read_line(&mut price).unwrap();
+                    let price: f64 = price.trim().parse().unwrap();
+    
+                    println!("Enter quantity: ");
+                    let mut quantity = String::new();
+                    io::stdin().read_line(&mut quantity).unwrap();
+                    let quantity: usize = quantity.trim().parse().unwrap();
+    
+                    let mpsc_status = server_tx.send(Query::Sell(symbol, Order::new(creator_id, price, quantity))).await;
+                    println!("Sell order added.");
+                    mpsc_status
+                }
+                "exit" => {
+                    break;
+                }
+                _ => {
+                    println!("Invalid command\n");
+                    continue;
+                }
+            };
 
-                v.add_buy_order(Order::new(creator_id, price, quantity));
-                println!("Buy order added.");
+            if let Err(e) = status {
+                eprintln!("Error: {:#?}", e);
+                continue;
             }
-            "sell" => {
-                println!("Enter creator id: ");
-                let mut creator_id = String::new();
-                io::stdin().read_line(&mut creator_id).unwrap();
-                let creator_id: usize = creator_id.trim().parse().unwrap();
 
-                println!("Enter price: ");
-                let mut price = String::new();
-                io::stdin().read_line(&mut price).unwrap();
-                let price: f64 = price.trim().parse().unwrap();
-
-                println!("Enter quantity: ");
-                let mut quantity = String::new();
-                io::stdin().read_line(&mut quantity).unwrap();
-                let quantity: usize = quantity.trim().parse().unwrap();
-
-                v.add_sell_order(Order::new(creator_id, price, quantity));
-                println!("Sell order added.");
-            }
-            "resolve" => {
-                let trades = v.resolve();
-                println!("Trades: {:#?}", trades);
-            }
-            "exit" => {
-                break;
-            }
-            _ => {
-                println!("Invalid command");
+            match server_rx.recv().await {
+                Some(response) => {
+                    match response {
+                        QueryResponse::OrderPosted => {
+                            println!("Order posted.");
+                        },
+                        QueryResponse::SymbolNotFound => {
+                            println!("Symbol not found.");
+                        },
+                        QueryResponse::OHLC(open, high, low, close) => {
+                            println!("OHLC: {:#?}", (open, high, low, close));
+                        },
+                        QueryResponse::QueriedOrders(orders) => {
+                            println!("Orders: {:#?}", orders);
+                        }
+                    }
+                },
+                None => {
+                    break;
+                }
             }
         }
+    });
+
+
+    let mut tick_interval = time::interval(time::Duration::from_millis(TICK_INTERVAL_MILLISECS));
+    tick_interval.set_missed_tick_behavior(time::MissedTickBehavior::Skip);
+
+    'market_loop: loop {
+        tick_interval.tick().await;
+        loop {
+            for stock in stocks.values_mut() {
+                let executed_trades = stock.resolve();
+                for trade in executed_trades {
+                    market_speak(format!("Market says> Trade executed: {:#?}", trade), &mut stdout, false);
+                }
+            };
+
+            match market_rx.try_recv() {
+                Ok(query) => {
+                    let status = match query {
+                        Query::Buy(symbol, order) => {
+                            if let Some(stock) = stocks.get_mut(&symbol) {
+                                stock.add_buy_order(order);
+                                market_tx.send(QueryResponse::OrderPosted).await
+                            } else {
+                                market_tx.send(QueryResponse::SymbolNotFound).await
+                            }
+                        },
+                        Query::Sell(symbol, order) => {
+                            if let Some(stock) = stocks.get_mut(&symbol) {
+                                stock.add_sell_order(order);
+                                market_tx.send(QueryResponse::OrderPosted).await
+                            } else {
+                                market_tx.send(QueryResponse::SymbolNotFound).await
+                            }
+                        },
+                        Query::OHLC(symbol) => {
+                            if let Some(stock) = stocks.get(&symbol) {
+                                let (open, high, low, close) = stock.get_ohlc();
+                                market_tx.send(QueryResponse::OHLC(open, high, low, close)).await
+                            } else {
+                                market_tx.send(QueryResponse::SymbolNotFound).await
+                            }
+                        },
+                        Query::BuyOrders(symbol) => {
+                            if let Some(stock) = stocks.get(&symbol) {
+                                market_tx.send(QueryResponse::QueriedOrders(stock.get_buy_orders())).await
+                            } else {
+                                market_tx.send(QueryResponse::SymbolNotFound).await
+                            }
+                        },
+                        Query::SellOrders(symbol) => {
+                            if let Some(stock) = stocks.get(&symbol) {
+                                market_tx.send(QueryResponse::QueriedOrders(stock.get_sell_orders())).await
+                            } else {
+                                market_tx.send(QueryResponse::SymbolNotFound).await
+                            }
+                        }
+                    };
+
+                    if let Err(e) = status {
+                        market_speak(format!("Error: {:#?}", e), &mut stdout, true);
+                    }
+                },
+                Err(TryRecvError::Empty) => {
+                    break;
+                },
+                Err(TryRecvError::Disconnected) => {
+                    break 'market_loop;
+                }
+            }
+        }
+    }
+
+    if let Err(server_status) = server.await {
+        eprintln!("Error: {:#?}", server_status);
+    } else {
+        println!("Bbye!");
+    }
+}
+
+/// Prints a message to the terminal with a different colour for the market.
+/// 
+/// This colour is defined by `MARKET_OUTPUT_COLOUR`.
+fn market_speak(message: String, stdout: &mut StandardStream, error: bool) {
+    if let Err(e) = stdout.set_color(ColorSpec::new().set_fg(Some(MARKET_OUTPUT_COLOUR))) {
+        println!("Could not set terminal color({:#?}). The next statement is probably from the Market:", e);
+    }
+
+    if error {
+        eprintln!("{}", message);
+    } else {
+        println!("{}", message);
+    }
+
+    if let Err(e) = stdout.reset() {
+        println!("Could not set terminal color({:#?}). Market output ends.", e);
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,17 +1,29 @@
+use std::collections::HashMap;
 use chrono::{DateTime, Utc};
 
 // 10 raised to the number of decimals to keep for prices.
 const PRICE_PRECISION_FACTOR: f64 = 1e2;
+/// Number of unique prices that are checked for in the order book.
+const NO_OF_PRICES_QUERIED: usize = 5;
 
+/// Holds details for a stock and its orders.
 pub struct Stock {
+    /// The symbol of the stock (e.g., "ORT").
+    /// 
+    /// This is used for querying the stock, and so must be unique.
     symbol: String,
+    /// The full name of the stock (e.g., "Orchard de Rosa et Tulipan")
     name: String,
+    /// Buy orders for the stock.
     buy_orders: Vec<Order>,
+    /// Sell orders for the stock.
     sell_orders: Vec<Order>,
+    /// Open, high, low, close prices for the stock.
     ohlc: OHLC,
 }
 
 impl Stock {
+    /// Creates a new stock with the given symbol and name.
     pub fn new(symbol: &str, name: &str) -> Self {
         Self {
             symbol: symbol.to_string(),
@@ -22,24 +34,71 @@ impl Stock {
         }
     }
 
+    /// Returns the symbol of the stock.
     pub fn get_symbol(&self) -> &str {
         &self.symbol
     }
 
+    /// Returns the name of the stock.
     pub fn get_name(&self) -> &str {
         &self.name
     }
 
+    /// Adds a buy order to the stock.
     pub fn add_buy_order(&mut self, order: Order) {
         self.buy_orders.push(order);
         self.sort_orders();
     }
 
+    /// Adds a sell order to the stock.
     pub fn add_sell_order(&mut self, order: Order) {
         self.sell_orders.push(order);
         self.sort_orders();
     }
 
+    /// Returns pending buy orders for the stock.
+    pub fn get_buy_orders(&self) -> Vec<(f64, usize)> {
+        let mut pricelist = HashMap::<usize, usize>::new();
+
+        for order in &self.buy_orders {
+            let price = order.get_unadjusted_price();
+            let quantity = order.get_quantity();
+
+            if let Some(existing_price) = pricelist.get(&price) {
+                pricelist.insert(price, existing_price + quantity);
+            } else {
+                if pricelist.len() >= NO_OF_PRICES_QUERIED {
+                    break;
+                }
+                pricelist.insert(price, quantity);
+            }
+        }
+
+        pricelist.iter().map(|(price, quantity)| ((*price as f64) / PRICE_PRECISION_FACTOR, *quantity)).collect()
+    }
+
+    /// Returns pending sell orders for the stock.
+    pub fn get_sell_orders(&self) -> Vec<(f64, usize)> {
+        let mut pricelist = HashMap::<usize, usize>::new();
+
+        for order in &self.sell_orders {
+            let price = order.get_unadjusted_price();
+            let quantity = order.get_quantity();
+
+            if let Some(existing_price) = pricelist.get(&price) {
+                pricelist.insert(price, existing_price + quantity);
+            } else {
+                if pricelist.len() >= NO_OF_PRICES_QUERIED {
+                    break;
+                }
+                pricelist.insert(price, quantity);
+            }
+        }
+
+        pricelist.iter().map(|(price, quantity)| ((*price as f64) / PRICE_PRECISION_FACTOR, *quantity)).collect()
+    }
+
+    /// Resolves trades between buy and sell orders.
     pub fn resolve(&mut self) -> Vec<Trade> {
         let mut trades = Vec::new();
 
@@ -88,26 +147,36 @@ impl Stock {
         trades
     }
 
+    /// Sorts buy and sell orders by price.
     fn sort_orders(&mut self) {
         self.buy_orders.sort_by(|a, b| b.price.partial_cmp(&a.price).expect("prices are f64s and should be comparable."));
         self.sell_orders.sort_by(|a, b| a.price.partial_cmp(&b.price).expect("prices are f64s and should be comparable."));
     }
 
+    /// Returns the open, high, low, close prices for the stock.
     pub fn get_ohlc(&self) -> (Option<f64>, Option<f64>, Option<f64>, Option<f64>) {
         self.ohlc.get()
     }
 }
 
+/// An order to buy or sell a stock.
 pub struct Order {
+    /// The ID of the creator of the order.
     creator_id: usize,
-    price: f64,
+    /// The price per stock.
+    price: usize,
+    /// The quantity of the order.
     quantity: usize,
+    /// The time the order was created.
+    /// 
+    /// The price listed on the order that was created earlier is considered while resolving orders.
     time: DateTime<Utc>,
 }
 
 impl Order {
+    /// Creates a new order with the given creator ID, price, and quantity.
     pub fn new(creator_id: usize, price: f64, quantity: usize) -> Self {
-        let price = (price * PRICE_PRECISION_FACTOR).trunc() / PRICE_PRECISION_FACTOR;
+        let price = (price * PRICE_PRECISION_FACTOR) as usize;
 
         Self {
             creator_id,
@@ -117,36 +186,52 @@ impl Order {
         }
     }
 
+    /// Returns the total value of the order.
     pub fn get_value(&self) -> f64 {
-        self.price * self.quantity as f64
+        (self.price as f64) * (self.quantity as f64) / PRICE_PRECISION_FACTOR
     }
 
+    /// Returns the price per stock of the order.
     pub fn get_price(&self) -> f64 {
+        self.price as f64 / PRICE_PRECISION_FACTOR
+    }
+
+    /// Returns the price per stock WITHOUT adjusting for the precision factor.
+    fn get_unadjusted_price(&self) -> usize {
         self.price
     }
 
+    /// Returns the quantity of the order.
     pub fn get_quantity(&self) -> usize {
         self.quantity
     }
 
+    /// Returns the time the order was created.
     pub fn get_time(&self) -> DateTime<Utc> {
         self.time
     }
 
+    /// Reduces the quantity of the order by the given amount.
     pub fn resolve(&mut self, quantity: usize) {
         self.quantity -= quantity;
     }
 }
 
 #[derive(Debug)]
+/// A log of a resolved trade between a buyer and a seller.
 pub struct Trade {
+    /// The ID of the buyer.
     buyer_id: usize,
+    /// The ID of the seller.
     seller_id: usize,
+    /// The price per stock.
     price: f64,
+    /// The quantity of the trade.
     quantity: usize,
 }
 
 impl Trade {
+    /// Creates a new trade with the given buyer ID, seller ID, price, and quantity.
     fn new(buyer_id: usize, seller_id: usize, price: f64, quantity: usize) -> Self {
         Self {
             buyer_id,
@@ -157,6 +242,7 @@ impl Trade {
     }
 }
 
+/// Open, high, low, close prices for a stock.
 pub struct OHLC {
     open: Option<f64>,
     high: Option<f64>,
@@ -165,6 +251,9 @@ pub struct OHLC {
 }
 
 impl OHLC {
+    /// Creates a blank OHLC struct.
+    /// 
+    /// Values are all set to `None` until the first update.
     fn new() -> Self {
         Self {
             open: None,
@@ -174,17 +263,50 @@ impl OHLC {
         }
     }
 
-    fn update(&mut self, price: f64) {
+    /// Updates the OHLC values according to the latest trade price provided.
+    fn update(&mut self, latest_price: f64) {
         if self.open.is_none() {
-            self.open = Some(price);
+            self.open = Some(latest_price);
         }
 
-        self.high = Some(self.high.unwrap_or(price).max(price));
-        self.low = Some(self.low.unwrap_or(price).min(price));
-        self.close = Some(price);
+        self.high = Some(self.high.unwrap_or(latest_price).max(latest_price));
+        self.low = Some(self.low.unwrap_or(latest_price).min(latest_price));
+        self.close = Some(latest_price);
     }
 
+    /// Returns the open, high, low, close prices.
     pub fn get(&self) -> (Option<f64>, Option<f64>, Option<f64>, Option<f64>) {
         (self.open, self.high, self.low, self.close)
     }
+}
+
+/// A query to the market.
+pub enum Query {
+    /// Post a buy order for the stock.
+    Buy(String, Order),
+    /// Post a sell order for the stock.
+    Sell(String, Order),
+    /// Query the OHLC prices for the stock.
+    OHLC(String),
+    /// Query the pending buy orders for the stock.
+    BuyOrders(String),
+    /// Query the pending sell orders for the stock.
+    SellOrders(String),
+}
+
+/// A response from the market to a query.
+pub enum QueryResponse {
+    // Successes
+    /// The order was successfully posted.
+    OrderPosted,
+    /// A vector of pending orders for the stock.
+    /// 
+    /// It contains a limited number of unique prices and their quantities. The number of unique prices is defined by `NO_OF_PRICES_QUERIED`.
+    QueriedOrders(Vec<(f64, usize)>),
+    /// The open, high, low, close prices for the stock.
+    OHLC(Option<f64>, Option<f64>, Option<f64>, Option<f64>),
+    
+    // Errors
+    /// The symbol provided was not found.
+    SymbolNotFound,
 }
